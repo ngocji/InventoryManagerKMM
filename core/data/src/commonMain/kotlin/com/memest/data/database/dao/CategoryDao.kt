976b78152b33bdfa7e7892368d1b0877f07a4031
dvngoc <dvngoc@mz.co.kr>
2024-12-04 14:20:29 +0700

    Update dao

--- core/data/src/commonMain/kotlin/com/memest/data/database/dao/CategoryDao.kt
+++ core/data/src/commonMain/kotlin/com/memest/data/database/dao/CategoryDao.kt
@@ -1,6 +1,9 @@
 package com.memest.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.memest.data.database.entity.CategoryEntity
 import kotlinx.coroutines.flow.Flow
@@ -8,5 +11,17 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface CategoryDao {
     @Query("SELECT * FROM categories order by updated_timestamp desc")
-    fun getCategories():Flow<List<CategoryEntity>>
+    fun getCategories(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun inserts(vararg category: CategoryEntity)
+
+    @Delete
+    suspend fun delete(category: CategoryEntity)
+
+    @Query("DELETE FROM categories where id = :id")
+    suspend fun deleteById(id: String)
+
+    @Query("select * from categories where id = :id")
+    fun getCategory(id: String): Flow<CategoryEntity?>
 }
